63d466009f06e1bde39a67b4f0fc715e79c3dede
Sejmou <samo.kolter@gmail.com>
2023-05-01 18:46:22 +0200

    figure out proper setup for app with native module deps

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { requestMIDIAccess } from '@motiz88/react-native-midi';
+import { useEffect } from 'react';
 
 export default function App() {
+  useEffect(() => {
+    requestMIDIAccess().then(midiAccess => {
+      // Use midiAccess.inputs and midiAccess.outputs
+      console.log(midiAccess.inputs);
+    });
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
