c0441a98a17be4b8bdba19aff71022631a32b028
Sejmou <samo.kolter@gmail.com>
2023-05-09 00:53:45 +0200

    add back app.config.js after deleting app.json by accident

---
+++ app.config.js
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+
+export default {
+  name: 'my-songbook',
+  slug: 'my-songbook',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.sejmou.mysongbook',
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+    package: 'com.sejmou.mysongbook',
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  extra: {
+    eas: {
+      projectId: 'eb37296c-489c-4ffb-b1b1-bd6dc86262c7',
+    },
+    databaseUrl: process.env.DATABASE_URL,
+  },
+  owner: 'sejmou',
+};
