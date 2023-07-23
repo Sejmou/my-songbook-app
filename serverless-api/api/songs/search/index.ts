e586b13df3a4c9e07c81b561c9ce3a8567c8acc5
Sejmou <samo.kolter@gmail.com>
2023-07-23 19:14:17 +0200

    api: add search endpoint, handle env vars better

--- serverless-api/api/songs/search/index.ts
+++ serverless-api/api/songs/search/index.ts
@@ -1,24 +1,20 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { z } from 'zod';
-import { superSecretKey } from '../../../db/api_base';
-
-import chromium from 'chrome-aws-lambda';
-
-// const browser = await puppeteer.launch( { args: ['--no-sandbox'] } );
+import { env } from '../../../db/api_base';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const method = req.method;
   const bearer = req.headers.authorization;
   const token = bearer?.split(' ')[1];
-  if (!token || token !== superSecretKey) {
+  if (!token || token !== env.SECRET_KEY) {
     return res.status(401).json({ message: 'Not authorized' });
   }
 
   if (method === 'GET') {
     try {
       const query = z.string().parse(req.query.q);
-      await getLyricsSearchResults(query);
-      return res.status(200).json({});
+      const searchResults = await getLyricsSearchResults(query);
+      return res.status(200).json(searchResults);
     } catch (e) {
       console.log(e);
       return res
@@ -31,34 +27,26 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 }
 
 async function getLyricsSearchResults(query: string) {
-  const localChromiumPath =
-    process.env.NODE_ENV == 'development'
-      ? await import('puppeteer').then(p => p.executablePath())
-      : undefined;
-  const browser = await chromium.puppeteer.launch({
-    args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
-    defaultViewport: chromium.defaultViewport,
-    executablePath:
-      process.env.NODE_ENV !== 'development'
-        ? await chromium.executablePath
-        : localChromiumPath,
-    headless: process.env.NODE_ENV !== 'development' ? chromium.headless : true,
-    ignoreHTTPSErrors: true,
-  });
-  const page = await browser.newPage();
+  const url = `https://api.genius.com/search?q=${query}`;
+  const options = {
+    headers: {
+      Authorization: `Bearer ${env.GENIUS_ACCESS_TOKEN}`,
+    },
+  };
+  const response = await fetch(url, options);
+  const json = await response.json();
 
-  await page.goto(`https://genius.com/search?q=${query}`);
+  const searchResults = json.response.hits;
+  const songs = searchResults
+    .filter((sr: any) => sr.type == 'song')
+    .map((result: any) => {
+      const song = result.result;
+      const title = song.title;
+      const artists = song.artist_names;
+      const thumbnail = song.song_art_image_thumbnail_url;
+      const url = song.url;
+      return { title, artists, thumbnail, url };
+    });
 
-  const songSearchResults = await page.$$eval(
-    'search-result-items',
-    el =>
-      Array.from(el[1].querySelectorAll('a')).map(el => ({
-        title: el.querySelector('.mini_card-title')?.textContent?.trim(),
-        artist: el.querySelector('.mini_card-subtitle')?.textContent?.trim(),
-        link: el.getAttribute('href'),
-      })) // second element is the song results, each result item is wrapped in an anchor tag
-  );
-  console.log('found songs', songSearchResults);
-  await browser.close();
-  return songSearchResults;
+  return songs;
 }
--- serverless-api/db/api_base.ts
+++ serverless-api/db/api_base.ts
@@ -1,9 +1,39 @@
 import { Pool } from 'pg';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
+// I you find a cleaner way to validate environment variables, please let me know!
+type EnvironmentVariables = {
+  GENIUS_ACCESS_TOKEN: string;
+  DATABASE_URL: string;
+  SECRET_KEY: string;
+};
+const expectedEnvVars = [
+  'GENIUS_ACCESS_TOKEN',
+  'DATABASE_URL',
+  'SECRET_KEY',
+] as const;
+
+function validateEnvironmentVariables(): void {
+  const missingVars = expectedEnvVars.filter(envVar => !process.env[envVar]);
+
+  if (missingVars.length > 0) {
+    throw new Error(`Missing environment variables: ${missingVars.join(', ')}`);
+  }
+}
+
+validateEnvironmentVariables();
+
+function createEnvObject(): EnvironmentVariables {
+  return expectedEnvVars.reduce(
+    (acc, envVar) => ({ ...acc, [envVar]: process.env[envVar] }),
+    {} as EnvironmentVariables
+  );
+}
+
+export const env: EnvironmentVariables = createEnvObject();
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
 export const db = drizzle(pool);
-export const superSecretKey = process.env.SUPER_SECRET_KEY;
